3a229e9b3441e034aa63a9ca252af745cb7988f6
Harvey Asprilla <kikiyam_8@hotmail.com>
2025-03-28 22:46:36 -0500

    url

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductoController;
 
-Route::routes('product', ProductoController::class);
+Route::apiResource('products', ProductoController::class);
